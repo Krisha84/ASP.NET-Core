c17cd45aed76dee63695f72e0b134fa1beb80c2b
Krisha Busa <23010101039@darshan.ac.in>
2024-12-31 20:44:25 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestA/Employee7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleAppTest
+{
+    abstract class Employee7
+    {
+        public string name { get; set; }
+        public int employeeID { get; set; }
+        public abstract void CalculateSalary(int hoursWorked, double hourlyRate);
+        public abstract void DisplayEmployeeDetails();
+    }
+    class FullTimeEmployee : Employee7
+    {
+        public double salary { get; set; }
+        public override void CalculateSalary(int hoursWorked, double hourlyRate)
+        {
+            salary = hoursWorked * hourlyRate;
+        }
+        public override void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("Employee ID: " + employeeID);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Salary: " + salary);
+        }
+
+    }
+}
