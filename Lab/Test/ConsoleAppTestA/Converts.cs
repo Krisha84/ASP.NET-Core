24e4aac7ded02304a34ed8147ad76aa69d05026b
Krisha Busa <23010101039@darshan.ac.in>
2024-12-31 20:43:42 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestA/Converts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    internal class Converts
+    {
+        public double km;
+        public double pounds;
+        public double liters;
+
+        public void kmToMiles(double km)
+        {
+            Console.WriteLine("Miles : " + (km * 0.621371));
+        }
+
+        public void poundsToKg(double pounds)
+        {
+            Console.WriteLine("Kilograms : " + (pounds * 0.453592));
+        }
+
+        public void litersToGallons(double liters)
+        {
+            Console.WriteLine("Gallons : " + (liters * .264172));
+        }
+    }
+}
