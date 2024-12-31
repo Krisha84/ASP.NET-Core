2e4d8722160a8e6ce37d522221e8e46a4e30f789
Krisha Busa <23010101039@darshan.ac.in>
2024-12-31 20:43:22 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestA/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    internal class Calculator
+    {
+        public int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return (a * b);
+        }
+
+        public float Multiply(float a, float b)
+        {
+            return (a * b);
+        }
+
+    }
+}
