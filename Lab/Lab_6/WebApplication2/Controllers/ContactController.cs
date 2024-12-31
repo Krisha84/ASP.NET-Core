246e63e66a25ff35790bd35ba99e3c5b1470530e
Krisha Busa <23010101039@darshan.ac.in>
2024-12-31 11:12:41 +0530

    Add files via upload

---
+++ Lab/Lab_6/WebApplication2/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Email()
+        {
+            return View();
+        }
+
+        public IActionResult SocialMedia()
+        {
+            return View();
+        }
+    }
+}
