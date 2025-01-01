478a2fcb82efc63553172ca39030b53515576755
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 21:38:06 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestF/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestF
+{
+    internal class ShoppingCart
+    {
+        private List<string> cart = new List<string>();
+
+        public void AddItem(string itemName, int quantity)
+        {
+            cart.Add("Item:" + itemName + " , Quantity:" + quantity);
+        }
+
+        public void AddItem(string itemName, int quantity, double price)
+        {
+            cart.Add("Item:" + itemName + " , Quantity:" + quantity + " , Price:" + price);
+        }
+
+        public void AddItem(int itemCode, int quantity)
+        {
+            cart.Add("Item Code:" + itemCode + " , Quantity:" + quantity);
+        }
+
+        public void DisplayCart()
+        {
+            foreach (var item in cart)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
