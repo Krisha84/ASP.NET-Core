6bdaf3aef8c199abd2e8dfe3ea28bf9dd6437bdc
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 22:07:56 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestE/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestE
+{
+    abstract class Employee
+    {
+        public string Name;
+        public int EmployeeID;
+        public abstract double CalculateSalary();
+    }
+
+    interface IBonus
+    {
+        double CalculateBonus();
+    }
+
+    class Manager : Employee, IBonus
+    {
+        public double BaseSalary;
+        public double Allowances;
+
+        public override double CalculateSalary()
+        {
+            return BaseSalary + Allowances;
+        }
+
+        public double CalculateBonus()
+        {
+            return (BaseSalary * 0.10);
+        }
+    }
+
+    class Worker : Employee, IBonus
+    {
+        public double HourlyRate;
+        public int HoursWorked;
+
+        public override double CalculateSalary()
+        {
+            return (HourlyRate * HoursWorked);
+        }
+
+        public double CalculateBonus()
+        {
+            return (HourlyRate * 20);
+        }
+    }
+}
