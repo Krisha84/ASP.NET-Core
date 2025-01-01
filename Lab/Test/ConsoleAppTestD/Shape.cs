34ad2e2bf957bd37d6ab95fcaa73fb372da09017
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 21:06:12 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestD/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestD
+{
+    internal class Shape
+    {
+        public double Area(double radius) => Math.PI * radius * radius;
+        public double Area(double length, double width) => length * width;
+        public double Area(float baseLength, float height) => 0.5 * baseLength * height;
+    }
+}
