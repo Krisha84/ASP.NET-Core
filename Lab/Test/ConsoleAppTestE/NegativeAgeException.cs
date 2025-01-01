49edfc5423fb414a7179e06a362a6e890e7f61cf
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 22:08:18 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestE/NegativeAgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestE
+{
+    internal class NegativeAgeException : Exception
+    {
+        public NegativeAgeException(string message) : base(message) { }
+
+    }
+}
