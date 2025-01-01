2b92d03c925cb1d61e8e5d9869e518e07d76607c
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 20:41:00 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestC/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestC
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        
+        public Product(int productID, string productName, decimal price)
+        {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.Price = price;
+        }
+        
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine("--Product Details--");
+            Console.WriteLine("Product ID : " + ProductID);
+            Console.WriteLine("Product Name : " + ProductName);
+            Console.WriteLine("Price : " + Price);
+        }
+    }
+}
