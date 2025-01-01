b54cf63001d6952895b999accc5e44902699b4e4
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 21:36:48 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestF/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestF
+{
+    internal class Book
+    {
+        public int BookID;
+        public string Title;
+        public string Author;
+        public Book(int bookID, string title, string author)
+        {
+            this.BookID = bookID;
+            this.Title = title;
+            this.Author = author;
+        }
+        public void DisplayBookDetails()
+        {
+            Console.WriteLine("Book ID : " + BookID + " , Title : "  + Title + " , Author : " + Author);
+        }
+    }
+}
