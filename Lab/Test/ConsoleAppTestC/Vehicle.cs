6f4985b4c6a49935f1ab5fdfb3b0a5ed06654ea5
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 20:41:26 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestC/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestC
+{
+    internal interface Vehicle
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class Car : Vehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("The car has started !!");
+        }
+        
+        public void Stop()
+        {
+            Console.WriteLine("The car has stopped !!");
+        }
+    }
+
+}
