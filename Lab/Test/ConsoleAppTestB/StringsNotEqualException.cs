e6409e08e1388107c0634dbbf0c478d862bfcc3b
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 19:10:46 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestB/StringsNotEqualException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestB
+{
+    class StringsNotEqualException : Exception
+    {
+        public StringsNotEqualException(string message) : base(message) { }
+    }
+}
