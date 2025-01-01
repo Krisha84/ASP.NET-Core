b7a8e67c411a9290df8a13c415ff6b5d806f6a6a
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 21:37:47 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestF/Shape.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestF
+{
+    abstract class Shape
+    {
+        public string Name;
+        public string Dimensions;
+        public abstract double CalculateArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius;
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+            Dimensions = "Radius : " + radius;
+        }
+        public override double CalculateArea()
+        {
+            return (Math.PI * Radius * Radius);
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length;
+        public double Width;
+        public Rectangle(double length, double width)
+        {
+            Name = "Rectangle";
+            Length = length;
+            Width = width;
+            Dimensions = "Lengthn: " + length + ", Width : " + width;
+        }
+
+        public override double CalculateArea()
+        {
+            return (Length * Width);
+        }
+    }
+}
