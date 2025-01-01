7dd9ae0b66b5863ba7237c09acaab80a8e37090d
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 21:07:10 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestD/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestD
+{
+    internal class User
+    {
+        public string Name;
+        public int Age;
+        public string Email;
+        public string City;
+        public User(string name, int age, string email, string city)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Email = email;
+            this.City = city;
+        }
+        public void UpdateDetails(string name, int age, string email, string city)
+        {
+            Name = name;
+            Age = age;
+            Email = email;
+            City = city;
+        }
+    }
+}
