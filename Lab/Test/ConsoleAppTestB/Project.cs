c39144b2459e28f727df3701fd71ff51f42d6e27
Krisha Busa <23010101039@darshan.ac.in>
2025-01-01 19:09:53 +0530

    Add files via upload

---
+++ Lab/Test/ConsoleAppTestB/Project.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestB
+{
+    internal class Project
+    {
+        public string ProjectName { get; set; }
+        public int LineOfCode { get; set; }
+        public string FrontendTechnology { get; set; }
+        public string BackendTechnology { get; set; }
+        
+        public void GetDetails()
+        {
+            Console.Write("Enter Project Name: ");
+            ProjectName = Console.ReadLine();
+            Console.Write("Enter Line of Code: ");
+            LineOfCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Frontend Technology: ");
+            FrontendTechnology = Console.ReadLine();
+            Console.Write("Enter Backend Technology: ");
+            BackendTechnology = Console.ReadLine();
+        }
+   
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Project Name : " + ProjectName);
+            Console.WriteLine("Line of Code : " + LineOfCode);
+            Console.WriteLine("Frontend Technology : " + FrontendTechnology);
+            Console.WriteLine("Backend Technology : " + BackendTechnology);
+        }
+
+    }
+}
