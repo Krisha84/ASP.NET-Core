519189d6a5f8f32bb02fce19b2f9f3aadec4a6cd
Krisha Busa <23010101039@darshan.ac.in>
2025-02-13 22:03:38 +0530

    Add file via upload

--- Lab/WebApplicationTheme2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationTheme2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
